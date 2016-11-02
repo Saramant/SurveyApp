73ea24e5113092db8de6ee8a172fe61a28d912e8
Sara Serpico <sara.serpico@manticle.com>
2016-11-02 15:39:57 +0100

    commit

--- app/src/main/java/com/example/saraserpico/surveyapp/ChooseSurvey.java
+++ app/src/main/java/com/example/saraserpico/surveyapp/ChooseSurvey.java
@@ -1,20 +1,20 @@
 package com.example.saraserpico.surveyapp;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TextView;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.design.widget.FloatingActionButton;
+        import android.support.design.widget.Snackbar;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.Toolbar;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.ArrayAdapter;
+        import android.widget.Button;
+        import android.widget.ListView;
+        import android.widget.Spinner;
+        import android.widget.TextView;
 
-import java.util.ArrayList;
+        import java.util.ArrayList;
 
 public class ChooseSurvey extends LoginActivity {
 
